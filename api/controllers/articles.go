2295db71a4208153e06aa298585e641836ae4b7b
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-03-31 18:27:33 +0100

    Created articles endpoint controller

---
+++ api/controllers/articles.go
@@ -0,0 +1,44 @@
+package controllers
+
+import(
+  "time"
+  "net/http"
+  "bgocms/models"
+  "bgocms/db"
+  "github.com/labstack/echo"
+)
+
+func GetArticles(c echo.Context) error {
+  dbc := c.Get("db").(*db.Client)
+  articles, total, dbErr := dbc.GetArticleRows()
+  if dbErr != nil {
+    return c.JSON(http.StatusInternalServerError, dbErr.Error())
+  }
+  return c.JSON(http.StatusOK, models.ArticleResponce{
+    Articles: articles,
+    Total: total,
+  })
+}
+
+
+func AddArticle(c echo.Context) error {
+  dbc := c.Get("db").(*db.Client)
+  newArticle := models.Article{}
+
+  bindErr := c.Bind(&newArticle)
+  if bindErr != nil {
+    return c.JSON(http.StatusBadRequest, bindErr.Error())
+  }
+
+  timestamp := time.Now()
+  newArticle.Created = timestamp
+  newArticle.Last_edited = timestamp
+
+  dbErr := dbc.InsertArticleRow(newArticle)
+  if dbErr != nil {
+    return c.JSON(http.StatusInternalServerError, dbErr.Error())
+  }
+ return c.String(http.StatusOK, "Added") 
+
+
+}
