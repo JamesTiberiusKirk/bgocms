82382501b182286b9117885255839be74c1e4285
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-06-03 19:00:52 +0100

    Added model for article author

--- api/models/article.go
+++ api/models/article.go
@@ -17,3 +17,14 @@ type ArticleResponce struct {
   Articles []Article  `json:"articles"`
   Total int           `json:"total"`
 }
+
+type ArticleAuthor struct {
+  ID          int       `json:"id" db:"id"`
+  AuthorId    int       `json:"author_id" db:"user_id"`
+  AuthorUname string    `json:"author_uname" db:"uname"`
+  Title       string    `json:"title" db:"title"`
+  Body        string    `json:"body" db:"body"`
+  Created     time.Time `json:"created" db:"created"`
+  Last_edited time.Time `json:"last_edited" db:"last_edited"`
+
+}
